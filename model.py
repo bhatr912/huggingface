d1039cc6d2081b5fde38b8fb202e7ee2ba6e1c70
bhatr912 <152839031+bhatr912@users.noreply.github.com>
2024-09-28 11:23:01 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -4,3 +4,5 @@ from transformers import pipeline
 def load_model():
     # Download a small model from Hugging Face
     return pipeline("fill-mask", model="distilbert-base-uncased")
+
+##https://chatgpt.com/share/66f798bf-b928-800e-a79b-74a2ae11ed46
